df99c59d78f4074d76f9e83a3d3559de1a8e6033
Pirlo220 <sgalvezsoler@gmail.com>
2016-03-01 22:54:20 +0100

    First Login attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-int main(int argc, char** argv) {
-	return 0;
+int main() {
+	string password1="password";
+	string password;
+	
+	cout << "Please type in your password: ";
+	cin >> password;
+	
+	if(password == password1){
+		cout << "Welcome !!";
+		cout<< "\n";
+		system("pause");
+		return EXIT_SUCCESS;
+	}
+	else {
+		cout << "Please type in the correct password: ";
+		cout << "\n";
+		cin >> password;
+	}
+	system("pause");
+	return EXIT_SUCCESS;
 }
