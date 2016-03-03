3dfb5662c488a3525aa2cf2d983fdfb5f6f4aa23
Pirlo220 <sgalvezsoler@gmail.com>
2016-03-03 23:33:16 +0100

    Password gets hidden when writng

--- main.cpp
+++ main.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
-	char c = (char) 0;
+	char c = ' ';
 	string password1="password";
 	string password;
-	
+
 	cout << "Please type in your password: ";
-	//cin >> password;
-	cout << "Valor de c " << c;
-	while(c!=10){
-		cout << "Dentro" << int(c);
-		c=getchar();
+	
+	while(c!=13){	
+		c=getch();
 		if(c!=13){
 			password += c;
 			cout << '*';
 		}		
 	}
 	if(password == password1){
+		cout<< "\n";
 		cout << "Welcome !!";
 		cout<< "\n";
 		system("pause");
 		return EXIT_SUCCESS;
 	}
 	else {
+		cout << "\n";
 		cout << "Please type in the correct password: ";
 		cout << "\n";
 		cin >> password;
