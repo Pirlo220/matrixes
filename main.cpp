d17757089743d9a7188a3d6c9f08a0ab122689a4
Pirlo220 <sgalvezsoler@gmail.com>
2016-03-03 23:16:25 +0100

    Trying to hide password while writing

--- main.cpp
+++ main.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 int main() {
+	char c = (char) 0;
 	string password1="password";
 	string password;
 	
 	cout << "Please type in your password: ";
-	cin >> password;
-	
+	//cin >> password;
+	cout << "Valor de c " << c;
+	while(c!=10){
+		cout << "Dentro" << int(c);
+		c=getchar();
+		if(c!=13){
+			password += c;
+			cout << '*';
+		}		
+	}
 	if(password == password1){
 		cout << "Welcome !!";
 		cout<< "\n";
