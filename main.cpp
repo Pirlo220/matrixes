e4ddd68449cf5194bb94832661bac33b38c1da89
Pirlo220 <sgalvezsoler@gmail.com>
2016-03-09 00:08:59 +0100

    Refactoring

--- main.cpp
+++ main.cpp
@@ -5,46 +5,33 @@
 
 using namespace std;
 
+const string ACCESS = "password";
+bool isGranted(string mPass);
+
 int main() {
 	char c = ' ';
-	string password1="password";
-	string password;
-
+	string password;	
 	cout << "Please type in your password: \n";
-		
-	while(c!=13){	
+	do{		
 		c=getch();
-		if(c >= 33 && c <= 122){//[A-Z,a-z]
+		if(c==13){
+			cout << "\nPlease type in your password: \n";
+			password = "";
+		}
+		if(c >= 33 && c <= 122){//[0-9][A-Z,a-z]
 			password += c;
 			cout << '*';
 		}
-		else if (c == 8){
+		else if (c == 8){//backspace implementation
 			cout << '\b';
-			cout<<' ';
+			cout << ' ';
 			cout << '\b';
 			password = password.substr(0, password.size()-1);
-		}
-		if(c==13 ){
-				
-				
-			cout << password;
-			
-		}//
-		
-	}
-	if(password == password1){
-		cout<< "\n";
-		cout << "Welcome !!";
-		cout<< "\n";
-		system("pause");
-		return EXIT_SUCCESS;
-	}
-	else {
-		cout << "\n";
-		cout << "Please type in the correct password: ";
-		cout << "\n";
-		cin >> password;
-	}
-	system("pause");
+		}	
+	}while(!isGranted(password));	
 	return EXIT_SUCCESS;
+}
+
+bool isGranted(string mPass){
+	return (mPass == ACCESS);
 }
