7439af1ff827bc4aa0919005bbb6570c44af6ea2
Pirlo220 <sgalvezsoler@gmail.com>
2016-04-12 00:49:55 +0200

    Changed extension of header file. Implemented mock login function

--- login.cpp
+++ login.cpp
@@ -1,11 +1,17 @@
 
 #include <string>
-#include "login.h"
+#include "login.hpp"
+#include <iostream>
 
+#define PI 3.1415
 const std::string ACCESS = "password";
 
 int login(){
-
+	#ifdef PI
+		std::cout << "El valor PI es: " << PI << ": ten fe en el caos" << std::endl;
+	#else
+		std::cout << "PI no esta definido..." << endl;
+	#endif
 	return 1;
 }
 
