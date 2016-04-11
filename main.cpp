98e87a5648afc48eb25f7e1b4093f2aef088c27c
Pirlo220 <sgalvezsoler@gmail.com>
2016-04-11 23:30:33 +0200

    Commented current main implementation

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+const string ACCESS = "password";
+bool isGranted(string mPass);
+
+int main() {
+/*	char c = ' ';
+	string password;	
+	cout << "Please type in your password: \n";
+	do{		
+		c=getch();
+		if(c==13){
+			cout << "\nPlease type in your password: \n";
+			password = "";
+		}
+		if(c >= 33 && c <= 122){//[0-9][A-Z,a-z]
+			password += c;
+			cout << '*';
+		}
+		else if (c == 8){//backspace implementation
+			cout << '\b';
+			cout << ' ';
+			cout << '\b';
+			password = password.substr(0, password.size()-1);
+		}	
+	}while(!isGranted(password));	
+	return EXIT_SUCCESS;
+}
+
+bool isGranted(string mPass){
+	return (mPass == ACCESS);
+}*/
+}
