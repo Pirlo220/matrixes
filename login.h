886a00bcc843375aae16ed28a65d35fa9ae5aa63
Pirlo220 <sgalvezsoler@gmail.com>
2016-04-11 23:20:18 +0200

    Added login file header

---
+++ login.h
@@ -0,0 +1,9 @@
+#ifndef LOGIN_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define LOGIN_H
+
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+
+int login();
+
+#endif
