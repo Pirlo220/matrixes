e957267852e76434b17e8328e9f835ac93a5c79c
Pirlo220 <sgalvezsoler@gmail.com>
2016-04-17 23:54:38 +0200

    Deleted commented code in main

--- main.cpp
+++ main.cpp
@@ -8,31 +8,5 @@ using namespace std;
 
 int main() {
 	int p = login();
-/*	char c = ' ';
-	string password;	
-	cout << "Please type in your password: \n";
-	do{		
-		c=getch();
-		if(c==13){
-			cout << "\nPlease type in your password: \n";
-			password = "";
-		}
-		if(c >= 33 && c <= 122){//[0-9][A-Z,a-z]
-			password += c;
-			cout << '*';
-		}
-		else if (c == 8){//backspace implementation
-			cout << '\b';
-			cout << ' ';
-			cout << '\b';
-			password = password.substr(0, password.size()-1);
-		}	
-	}while(!isGranted(password));	
 	return EXIT_SUCCESS;
-}
-
-bool isGranted(string mPass){
-	return (mPass == ACCESS);
-}*/
-return EXIT_SUCCESS;
 }
