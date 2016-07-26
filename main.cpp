fc0eb666fb08bdb0d057b68962074b88ef054e7e
Pirlo220 <sgalvezsoler@gmail.com>
2016-07-26 19:33:52 +0200

    Added example for dynamic array

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// rememb-o-matic
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -6,31 +7,77 @@
 #include <iomanip>
 #include <cmath>
 #include <limits>
-
+#include <new>
 using namespace std;
 
 int main() {
-	int p = login();
-	if(p == 1){
+	int pass = login();
+	if(pass == 1){
 		//do{
-			cin.clear();
-			std::cout << "\nPlease try to overflow the buffer: \n";
-			float f;
-			f = 18446744073709551610u;
+			//cin.clear();
+			//std::cout << "\nPlease try to overflow the buffer: \n";
+			//float f;
+			//f = 18446744073709551610u;
 			//std::cin >> f; // let user put his name
 			//cout.setf(ios::fixed);
 			
 			/*
 			//executes loop if the input fails (e.g., no characters were read)
-while (std::cout << "Enter a number" && !(std::cin >> num)) {
+		while (std::cout << "Enter a number" && !(std::cin >> num)) {
     std::cin.clear(); //clear bad input flag
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
     std::cout << "Invalid input; please re-enter.\n";
-}
-			*/
-		 std::cout << std::setprecision(3) << f;
-			//cout << f;
-		//} while(1==1);		
+	}
+	*/
+	/*
+	int row = 1;
+	int col = 1;
+	std::cout << "Enter a row: ";	
+	cin >> row;
+	std::cout << "Enter a column: ";	
+	cin >> col;
+	float matrix [1][1];
+	std::cout << "Select a row: " << matrix;
+		std::cout << "matrix[] --> " << matrix[9][9];
+	int posRow;
+	int posCol;
+	float value = 0;
+	std::cout << "Select a row: ";
+	cin >> posRow;
+	std::cout << "Select a col: ";
+	cin >> posCol;
+	std::cout << "enter a value: ";
+	cin >> value;	
+	matrix[posRow][posCol] = value;
+	std::cout << "Select a row: " << matrix;
+	std::cout << "matrix[" << posRow  <<"][" << posCol << "] --> " << matrix[posRow][posCol];	
+	//std::cout << std::setprecision(3) << f;
+	//cout << f;
+	//} while(1==1);		
+	}*/
+
+
+//Example for dynamic memory
+//http://www.cplusplus.com/doc/tutorial/dynamic/
+  int i,n;
+  int * p;
+  cout << "How many numbers would you like to type? ";
+  cin >> i;
+  p= new (nothrow) int[i];
+  if (p == nullptr)
+    cout << "Error: memory could not be allocated";
+  else
+  {
+    for (n=0; n<i; n++)
+    {
+      cout << "Enter number: ";
+      cin >> p[n];
+    }
+    cout << "You have entered: ";
+    for (n=0; n<i; n++)
+      cout << p[n] << ", ";
+    delete[] p;
+  }
 	}
 	return EXIT_SUCCESS;
 }
