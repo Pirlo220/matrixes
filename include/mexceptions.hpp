08a61dadd326a6442f74607f588c358048df1683
Sergio Galvez <Pirlo220@users.noreply.github.com>
2016-08-21 11:19:51 +0200

    Changed returned messages.
    You don't have to tell that user is blocked, that will give a clue about an existing user. Better return a generic error.

--- include/mexceptions.hpp
+++ include/mexceptions.hpp
@@ -6,6 +6,6 @@ using namespace std;
 class LockedUserException: public exception{
 public:
   virtual const char* message() const throw(){
-    return "Your user is locked please contact administrators";
+    return "Internal Error: Contact Administrators";
   }
 };
