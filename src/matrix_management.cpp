e38b132e0bb2f81f0a1c84416d4154e2d37fb0bb
sgalvez <sgalvezsoler@gmail.com>
2016-08-25 20:10:24 +0200

    Function to create a folder

--- src/matrix_management.cpp
+++ src/matrix_management.cpp
@@ -8,10 +8,38 @@
 #include <cstdio>
 #include <fstream>
 
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <errno.h>
+int mkpath(std::string s,mode_t mode)
+{
+    size_t pre=0,pos;
+    std::string dir;
+    int mdret;
+
+    if(s[s.size()-1]!='/'){
+        // force trailing / so we can handle everything in loop
+        s+='/';
+    }
+
+    while((pos=s.find_first_of('/',pre))!=std::string::npos){
+        dir=s.substr(0,pos++);
+        pre=pos;
+        if(dir.size()==0) continue; // if leading / first time is 0 length
+        if((mdret=mkdir(dir.c_str(),mode)) && errno!=EEXIST){
+            return mdret;
+        }
+    }
+    return mdret;
+}
+
 void save_log(){
   std::ofstream outfile;
-
-  outfile.open("test.txt", std::ios_base::app);
+  int mkdirretval;
+  mkdirretval=mkpath("./logs",0755);
+  
+  outfile.open("./logs/test.txt", std::ios_base::app);
   outfile << "Data\n"; 
 }
 
