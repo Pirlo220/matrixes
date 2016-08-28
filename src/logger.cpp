395862ddb84ef9f6255d938da289c65c2971fcfc
sgalvez <sgalvezsoler@gmail.com>
2016-08-28 13:29:12 +0200

    hashing log content

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,10 +1,66 @@
 #include <logger.hpp>
 #include <sys/stat.h>
 #include <cstring>
+#include <stdio.h>
 #include <sodium.h>
-
+#include <pqxx/pqxx>
+#include <string>
+#include <fstream>
 //string get_file_content(string url);
 string get_content_hash(string data);
+#define MAXLEN 64
+#define MESSAGE ((const unsigned char *) "Arbitrary data to hash")
+#define MESSAGE_LEN 22
+
+using namespace std;
+
+string get_file_content(string url){
+  /* string line;
+  std::ifstream myfile (url.c_str());
+  if (myfile.is_open())
+    {
+      while ( getline (myfile,line) )
+	{
+	  cout << line << '\n';
+	}
+      myfile.close();
+    }
+
+  else cout << "Unable to open file"; 
+  */
+  std::ifstream ifs(url.c_str());
+  std::string content( (std::istreambuf_iterator<char>(ifs) ),
+		      (std::istreambuf_iterator<char>()    ) );
+ return content;
+}
+void update_log_hash(string hash, string log_name){
+  pqxx::connection c("dbname=matrixes user=matrixuser");
+  pqxx::work txn(c); 
+  txn.exec(
+	   "UPDATE logs "
+	   "SET hash = " + txn.quote(hash) +
+	   "WHERE log_name = " + txn.quote(log_name));
+
+  txn.commit();  
+}
+
+
+string get_log_hash(std::string log_name){
+  cout << "log_name " << log_name;
+
+  pqxx::connection c("dbname=matrixes user=matrixuser");
+  pqxx::work txn(c);
+
+  pqxx::result r = txn.exec(
+			    "SELECT hash " 
+			    "FROM logs " 			   
+			    "WHERE log_name=" + txn.quote(log_name));
+  string hash = "-1";
+  if(r.size() == 1){
+    hash = r[0][0].as<string>();
+  }
+  return hash;
+}
 
 int mkpath(std::string s,mode_t mode)
 {
@@ -27,22 +83,57 @@ int mkpath(std::string s,mode_t mode)
   }
   return mdret;
 }
+void insert_log_hash(string hash, string log_name){
+  pqxx::connection c("dbname=matrixes user=matrixuser");
+  pqxx::work txn(c); 
+  txn.exec(
+	   "INSERT INTO logs "
+	   " VALUES (" + txn.quote(log_name) + ", "// + txn.quote("sad") +
+	   + txn.quote(hash) + ")");//"'hashsss')");
+
+  txn.commit();  
+}
 
 void log(string data){
- 
+  cout << "logging data " << data << endl;
   std::ofstream outfile;
   int mkdirretval;
   mkdirretval=mkpath("./logs", 0755);
   string date =  UtilsLibrary::get_current_date_as_string();
   string url = "./logs/" + date;
-  //get_file_content(url);
   outfile.open(url.c_str(), std::ios_base::app);
-  cout << get_content_hash(data) << endl;
+ 
+  //cout << "hashed content" << get_content_hash(content);
   if(outfile.is_open()){
+    
     string info = "[" + UtilsLibrary::get_current_time_as_string() + "] :: " ;
     info += data ;
+    
+    cout << "loggin info " << info << endl;
     outfile << info << endl; 
     outfile.close();
+    string previous_hash = get_log_hash(url);
+    cout << "Existing hash " << previous_hash << endl;
+    if(previous_hash  == "-1"){
+      insert_log_hash(get_content_hash(info), url);    
+    } else { 
+      string content = get_file_content(url);
+      string hashed_content = get_content_hash(content);
+    
+      cout << "hashed previous content UNO " << hashed_content << endl;
+    
+
+
+      if(previous_hash != hashed_content){
+	cerr << "LOG FILE HAS BEEN MODIFIED";
+      } else {
+	
+	content += info;
+	//cout << "content with infor attached " << content;
+	update_log_hash(get_content_hash(content), url);
+      }      
+    }
+    
   } else {
     cerr << "Error al escribir en log" << endl;
   }  
@@ -54,29 +145,29 @@ void log_error(string data){
 }
 
 string get_content_hash(string data){
+  //cout << "Dentro de get content"<< endl;
+  //cout << data.length()+1 << endl;
   unsigned char hash[crypto_generichash_BYTES];
   const unsigned char *data_p = new unsigned char[data.length()+1];
+ 
+  data_p = (const unsigned char *)data.c_str();
+  // data_p[data.size()] = '\0'; // don't forget the terminating 0
 
-  crypto_generichash(hash, sizeof hash, data_p, sizeof data_p, NULL, 0);
   
-  std::string my_std_string(hash, hash + sizeof hash);
-  return my_std_string;
-}
-/*
-string get_file_content(string url){
-  string line;
-  ifstream myfile (url);
-  if (myfile.is_open())
-    {
-      while ( getline (myfile,line) )
-	{
-	  cout << line << '\n';
-	}
-      myfile.close();
-    }
+  cout << endl;
+  cout << "TAMAN " << data.length() << endl;
 
-  else cout << "Unable to open file"; 
+  // crypto_generichash(hash, sizeof hash, data_p, sizeof data_p, NULL, 0);
+  crypto_generichash(hash, sizeof hash,
+                   data_p, data.length()+1,
+                   NULL, 0);
+
+  ostringstream convert; 
+  for (size_t j = 0; j <sizeof hash; ++j) {
+    convert << ((unsigned int) hash[j]);
+  }
+  data_p = NULL;
+  delete data_p;
+  return convert.str(); 
 }
 
-
-*/
