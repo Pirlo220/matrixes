471f8f6e75c4bf80bb374ae29b6eb33e2e4ec64b
Sergio Galvez <Pirlo220@users.noreply.github.com>
2016-08-30 00:52:19 +0200

    Refactoring

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -4,8 +4,7 @@ template <typename T>
 Matrix<T>::Matrix(int rows = 1, int cols = 1, int ownerID=1, string name="empty")
   : mRows(rows), mCols(cols),mOwnerID(ownerID), mName(name)
 {
-  //mGrid = new T[mCols * mRows];
-  // mGrid.reserve(mCols * mRows);
+  mGrid.reserve(mCols * mRows);
   std::vector<float> v(mCols * mRows);
   mGrid = v;
   mID = -1;
@@ -15,25 +14,19 @@ Matrix<T>::Matrix(int rows = 1, int cols = 1, int ownerID=1, string name="empty"
 template <typename T>
 T& Matrix<T>::operator ()( int row, int column)
 {
-  //cout << "Accediendo ... " << (row * mCols) + column << endl;
   return mGrid.at((row * mCols) + column);
 }
 
 template <typename T>
 float Matrix<T>::assign (int row, int column, float value)
 {
-  // cout << "vector size" << mGrid.size() << endl;
-  // cout << "Asignando ... " << (row * mCols) + column << endl;
-  //mGrid[(row * mCols) + column] = value;
   mGrid[(row * mCols) + column] = value;
-   //cout << "ASIGNADO ... " <<  mGrid.at((row * mCols) + column) << endl;
 }
 
 template <typename T>
 Matrix<T>::~Matrix(void)
 {
- 
-  //delete[] mGrid;
+  mGrid.clear();
 }
 
 template <typename T>
@@ -71,19 +64,3 @@ void Matrix<T>::setID(int id)
 {
   mID = id;
 }
-/*
-template <typename T>
-void Matrix<T>::setContent(T *content)
-{
-  mGrid = content;
-}
-
-template <typename T>
-T* Matrix<T>::getContent()
-{
-  return mGrid;
-}
-*/
-
-
-
